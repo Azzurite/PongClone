b166db943b329ccaba269b4ca80d80f2cc4ab431
Azzurite <azzurite+git@gmail.com>
2014-12-13 09:03:26 +0100

    Add NotImplementedException
     - use in renderPaused

--- src/gamestate/GameState.cpp
+++ src/gamestate/GameState.cpp
@@ -77,6 +77,7 @@ void GameState::renderPaused(const Renderer& r) const
 {
 	if (!cachedRender_) {
 		// TODO impl
+		throw util::NotImplementedException{};
 //		renderImpl(r);
 //		SDL_Window* w = r.getWindow();
 //		SDL_Surface* windowSurface = SDL_GetWindowSurface(w);
---
+++ src/util/NotImplementedException.h
@@ -0,0 +1,84 @@
+/*! @file
+ *
+ * @date 13.12.2014
+ * @author Azzurite
+ *
+ * @copyright GPL v3
+ *   Copyright (C) 2014 Azzurite
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#pragma once
+
+#include <exception>
+
+namespace pong {
+namespace util {
+
+
+class NotImplementedException final : public std::exception
+{
+public:
+
+	/*!
+	 * @brief Default constructor.
+	 */
+	NotImplementedException() noexcept;
+
+	/*!
+	 * @brief Message constructor.
+	 * @param message the message to be shown by what()
+	 */
+	NotImplementedException(const char* message) noexcept;
+	
+	/*!
+	 * @brief Default copy constructor.
+	 */
+	NotImplementedException(const NotImplementedException&) noexcept;
+		
+	/*!
+	 * @brief Default move constructor.
+	 */
+	NotImplementedException(NotImplementedException&&) noexcept;
+	
+	/*!
+	 * @brief Default destructor.
+	 */
+	~NotImplementedException() noexcept;
+	
+	
+	/*!
+	 * @brief Default copy assignment operator.
+	 */
+	NotImplementedException& operator=(const NotImplementedException&) noexcept;
+	
+	/*!
+	 * @brief Default move assignment operator.
+	 */
+	NotImplementedException& operator=(NotImplementedException&&) noexcept;
+
+	virtual const char* what() const noexcept override { return message_; }
+
+protected:
+
+private:
+
+	const char* message_;
+
+};
+
+} // namespace pong
+} // namespace util
+
